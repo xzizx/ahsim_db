b1eddf6aae0234d0fade5473da60563b84cce71f
xzizx <113580927+xzizx@users.noreply.github.com>
2023-02-08 14:15:31 +0900

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,58 +23,99 @@ with st.sidebar:
     else :
         db_table = "AH8_cs156"
         
-    fl_sel = st.radio('Simulation Type?',('FL', 'FX'),index=0,horizontal=True)
-    if fl_sel == 'FL':
+    sim_sel = st.radio('Simulation Type?',('FL', 'FX'),index=0,horizontal=True)
+    if sim_sel == 'FL':
         test_case = "AH_AP1FL"
     else :
         test_case = "AH_AP1FX"
 
     op_sel = st.radio('Operation Type?',('O1', 'O2', 'O4','O8'),index=1,horizontal=True)
-    if op_sel == 'O1':
-        test_case = test_case+"_OP1"
+    if op_sel == 'O1': 
+        mode_sel='1Mx1'        
+        pri_sel='P0'
+        st.sidebar.text('Mode Type?')
+        st.sidebar.text(mode_sel)
+        st.sidebar.text('Primary ?')
+        st.sidebar.text(pri_sel)
+        test_case = test_case+"_OP1_1Mx1"
     elif op_sel == 'O2':
-        test_case = test_case+"_OP2"
+        mode_sel = st.radio('Mode Type?',('1Mx1','1Mx2','2Sx1','2Lx1'),index=2,horizontal=True)  
+        pri_sel  = st.radio('Primary ?',('P0', 'P1'),index=0,horizontal=True)     
+        if mode_sel == '1Mx1':
+            test_case = test_case+"_OP2_1Mx1"
+        elif mode_sel == '1Mx2':
+            test_case = test_case+"_OP2_1Mx2"
+        elif mode_sel == '2Sx1':
+            test_case = test_case+"_OP2_2Sx1"
+        else : 
+            test_case = test_case+"_OP2_2Lx1"   
     elif op_sel == 'O4':
-        test_case = test_case+"_OP4"
+        mode_sel = st.radio('Mode Type?',('1Mx1','1Mx2','2Sx1','2Lx1','1Mx4','2Sx2','2Lx2','4Sx1','4Lx1'),index=7,horizontal=True)
+        pri_sel  = st.radio('Primary ?',('P0', 'P1', 'P2', 'P3'),index=0,horizontal=True)        
+        if mode_sel == '1Mx1':
+            test_case = test_case+"_OP4_1Mx1"
+        elif mode_sel == '1Mx2':
+            test_case = test_case+"_OP4_1Mx2"
+        elif mode_sel == '2Sx1':
+            test_case = test_case+"_OP4_2Sx1"
+        elif mode_sel == '2Lx1':
+            test_case = test_case+"_OP4_2Lx1"   
+        elif mode_sel == '1Mx4':
+            test_case = test_case+"_OP4_1Mx4"    
+        elif mode_sel == '2Sx2':
+            test_case = test_case+"_OP4_2Sx2"
+        elif mode_sel == '2Lx2':
+            test_case = test_case+"_OP4_2Lx2"    
+        elif mode_sel == '4Sx1':
+            test_case = test_case+"_OP4_4Sx1"
+        else : 
+            test_case = test_case+"_OP4_4Lx1"             
     else: 
-        test_case = test_case+"_OP8"
-   
-    mode_sel = st.radio('Mode Type?',('1Mx1','1Mx2','1Mx4','2Sx1','2Lx1','2Sx2','2Lx2','4Sx1','4Lx1','1Mx8','2Sx4','2Lx4','8Sx1','8Lx1'),index=3,horizontal=True)
-    if mode_sel == '1Mx1':
-        test_case = test_case+"_1Mx1"
-    elif mode_sel == '1Mx2':
-        test_case = test_case+"_1Mx2"
-    elif mode_sel == '1Mx4':
-        test_case = test_case+"_1Mx4"    
-    elif mode_sel == '2Sx1':
-        test_case = test_case+"_2Sx1"
-    elif mode_sel == '2Lx1':
-        test_case = test_case+"_2Lx1"   
-    elif mode_sel == '2Sx2':
-        test_case = test_case+"_2Sx2"
-    elif mode_sel == '2Lx2':
-        test_case = test_case+"_2Lx2"    
-    elif mode_sel == '4Sx1':
-        test_case = test_case+"_4Sx1"
-    elif mode_sel == '4Lx1':
-        test_case = test_case+"_4Lx1"   
-    elif mode_sel == '1Mx8':
-        test_case = test_case+"_1Mx8"    
-    elif mode_sel == '2Sx4':
-        test_case = test_case+"_2Sx4"
-    elif mode_sel == '2Lx4':
-        test_case = test_case+"_2Lx4"   
-    elif mode_sel == '8Sx1':
-        test_case = test_case+"_8Sx1"
-    else: 
-        test_case = test_case+"_8Lx1"  
-        
-    pri_sel = st.radio('Primary ?',('P0', 'P1','P2', 'P3','P4', 'P5','P6', 'P7'),index=0,horizontal=True)
-    
-    comp_sel1 = st.selectbox('Compare FL/FX', ('FL', 'FX'),index=0)
-    comp_sel2 = st.selectbox('Compare Operating', ('O1', 'O2', 'O4', 'O8'),index=1)
-    comp_sel3 = st.selectbox('Compare Mode', ('1Mx1','1Mx2','1Mx4','2Sx1','2Lx1','2Sx2','2Lx2','4Sx1','4Lx1','1Mx8','2Sx4','2Lx4','8Sx1','8Lx1'),index=4)    
-    comp_sel4 = st.selectbox('Compare Primary', ('P0', 'P1','P2', 'P3','P4', 'P5','P6', 'P7'),index=0)    
+        mode_sel = st.radio('Mode Type?',('1Mx1','1Mx2','2Sx1','2Lx1','1Mx4','2Sx2','2Lx2','4Sx1','4Lx1','1Mx8','2Sx4','2Lx4','8Sx1','8Lx1'),index=12,horizontal=True)
+        pri_sel  = st.radio('Primary ?',('P0', 'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7'),index=0,horizontal=True)
+        if mode_sel == '1Mx1':
+            test_case = test_case+"_OP8_1Mx1"
+        elif mode_sel == '1Mx2':
+            test_case = test_case+"_OP8_1Mx2"
+        elif mode_sel == '2Sx1':
+            test_case = test_case+"_OP8_2Sx1"
+        elif mode_sel == '2Lx1':
+            test_case = test_case+"_OP8_2Lx1"   
+        elif mode_sel == '1Mx4':
+            test_case = test_case+"_OP8_1Mx4"    
+        elif mode_sel == '2Sx2':
+            test_case = test_case+"_OP8_2Sx2"
+        elif mode_sel == '2Lx2':
+            test_case = test_case+"_OP8_2Lx2"    
+        elif mode_sel == '4Sx1':
+            test_case = test_case+"_OP8_4Sx1"
+        elif mode_sel == '4Lx1':
+            test_case = test_case+"_OP8_4Lx1"   
+        elif mode_sel == '1Mx8':
+            test_case = test_case+"_OP8_1Mx8"    
+        elif mode_sel == '2Sx4':
+            test_case = test_case+"_OP8_2Sx4"
+        elif mode_sel == '2Lx4':
+            test_case = test_case+"_OP8_2Lx4"   
+        elif mode_sel == '8Sx1':
+            test_case = test_case+"_OP8_8Sx1"
+        else: 
+            test_case = test_case+"_OP8_8Lx1"         
+     
+    comp_sim = st.selectbox('Compare FL/FX', ('FL', 'FX'),index=0)
+    comp_op  = st.selectbox('Compare Operating', ('O1', 'O2', 'O4', 'O8'),index=1)
+    if comp_op == 'O1': 
+        comp_mode='1Mx1'        
+        comp_pri='P0'
+    elif comp_op == 'O2':
+        comp_mode= st.selectbox('Compare Mode', ('1Mx1','1Mx2','2Sx1','2Lx1'),index=3)    
+        comp_pri = st.selectbox('Compare Primary', ('P0', 'P1'),index=0)    
+    elif comp_op == 'O4':
+        comp_mode= st.selectbox('Compare Mode', ('1Mx1','1Mx2','2Sx1','2Lx1','1Mx4','2Sx2','2Lx2','4Sx1','4Lx1'),index=7)    
+        comp_pri = st.selectbox('Compare Primary', ('P0', 'P1', 'P2', 'P3'),index=0)    
+    else:     
+        comp_mode= st.selectbox('Compare Mode', ('1Mx1','1Mx2','2Sx1','2Lx1','1Mx4','2Sx2','2Lx2','4Sx1','4Lx1','1Mx8','2Sx4','2Lx4','8Sx1','8Lx1'),index=12)    
+        comp_pri = st.selectbox('Compare Primary', ('P0', 'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7'),index=0)    
 
 st.title(f'"{db_table}" C Simulation Result')
 
@@ -86,41 +127,20 @@ cnt_table = cur.fetchone()
 if cnt_table[0] == 0 :
     st.title(f':red[There is not "{db_table}" in C Simulation Result DB]')
 else:
-    #sql_query_table = f'SELECT id, time, fl, op, mode, mcs, pri, per FROM "{db_table}"'
-    #cur = db.execute(sql_query_table) 
-    #results = cur.fetchall()
-    st.write(f':red[PER TEST] Selected Parameter : AH_AP1:red[{fl_sel}]\_:red[{op_sel}]\_:red[{mode_sel}]\_MCSxx_:red[{pri_sel}]_LGI_STBCx_DOPx_SMTx_D256')  
-    st.write(f':blue[Compare] Selected Parameter : AH_AP1:blue[{comp_sel1}]\_:blue[{comp_sel2}]\_:blue[{comp_sel3}]\_MCSxx_:blue[{comp_sel4}]_LGI_STBCx_DOPx_SMTx_D256') 
+    st.write(f':red[PER TEST] Selected Parameter : AH_AP1:red[{sim_sel}]\_:red[{op_sel}]\_:red[{mode_sel}]\_MCSxx_:red[{pri_sel}]_LGI_STBCx_DOPx_SMTx_D256')  
+    st.write(f':blue[Compare] Selected Parameter : AH_AP1:blue[{comp_sim}]\_:blue[{comp_op}]\_:blue[{comp_mode}]\_MCSxx_:blue[{comp_pri}]_LGI_STBCx_DOPx_SMTx_D256') 
     
-    sql_mcs_per = f'SELECT DISTINCT mcs,per FROM "{db_table}" WHERE fl = "{fl_sel}" AND op = "{op_sel}" AND mode = "{mode_sel}" AND pri = "{pri_sel}" ORDER BY mcs '
+    sql_mcs_per = f'SELECT DISTINCT mcs,per FROM "{db_table}" WHERE fl = "{sim_sel}" AND op = "{op_sel}" AND mode = "{mode_sel}" AND pri = "{pri_sel}" ORDER BY mcs '
     selected_mcs_per = db.execute(sql_mcs_per)
     results_mcs_per  = selected_mcs_per.fetchall()
     
-    sql_comp_per = f'SELECT DISTINCT mcs,per FROM "{db_table}" WHERE fl = "{comp_sel1}" AND op = "{comp_sel2}" AND mode = "{comp_sel3}" AND pri = "{comp_sel4}" ORDER BY mcs '
+    sql_comp_per = f'SELECT DISTINCT mcs,per FROM "{db_table}" WHERE fl = "{comp_sim}" AND op = "{comp_op}" AND mode = "{comp_mode}" AND pri = "{comp_pri}" ORDER BY mcs '
     selected_comp_per = db.execute(sql_comp_per)
     results_comp_per  = selected_comp_per.fetchall()
     
-    if not results_mcs_per:        
-        if(op_sel=='O1'):
-            if(mode_sel=='1Mx1'):
-                st.title(f':red[Selected TEST is not TESTED, yet!]')         
-            else:
-                st.title(f':blue[Undefined MODE\!]')         
-        elif(op_sel=='O2'):       
-            if(mode_sel=='1Mx1' or mode_sel=='1Mx2' or mode_sel=='2Sx1' or mode_sel=='2Lx1'):
-                st.title(f':red[Selected TEST is not TESTED, yet!]')         
-            else:
-                st.title(f':blue[Undefined MODE\!]')         
-        elif(op_sel=='O4'):       
-            if(mode_sel=='1Mx1' or mode_sel=='1Mx2' or mode_sel=='1Mx4'or mode_sel=='2Sx1' or mode_sel=='2Lx1'or \
-               mode_sel=='2Sx2' or mode_sel=='2Lx2' or mode_sel=='4Sx1'or mode_sel=='4Lx1'):
-                st.title(f':red[Selected TEST is not TESTED, yet!]')         
-            else:
-                st.title(f':blue[Undefined MODE\!]')         
-        else :
-             st.write(f':red[Selected TEST is not TESTED, yet!]')                               
-
-    else : 
+    if not results_mcs_per: 
+        st.title(f':red[Selected TEST is not TESTED, yet!]')
+    else:
         df_snr = pd.DataFrame({'SNR': np.arange(0,30,0.5)})        
         df_per = pd.DataFrame(results_mcs_per)            
         column_name = ['SNR']
@@ -130,7 +150,7 @@ else:
             column_name.append(df_per[0][i])       
         df_snr.columns = column_name 
         df = df_snr.fillna(0.000000)            
-        
+            
         if results_comp_per :
             df_snr_comp = pd.DataFrame({'SNR': np.arange(0,30,0.5)})
             df_per_comp = pd.DataFrame(results_comp_per)
@@ -202,7 +222,7 @@ else:
             st.write(f':blue[Compare is not TESTED, yet!] or :blue[Undefined MODE\!]')               
         st.write(f'PER TEST RESULT:')  
         st.dataframe(df, use_container_width=False)
-        
+            
         if results_comp_per :                    
             st.write(f'Compare RESULT:')  
             st.dataframe(df_comp, use_container_width=False)
